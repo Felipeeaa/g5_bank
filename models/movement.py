ead1141674e8ed2fd4ab9a3543ac9f268d87bf2a
miguel <miturin09@gmail.com>
2025-12-16 14:09:04 +0100

    Commit Movement

--- models/movement.py
+++ models/movement.py
@@ -13,11 +13,10 @@ class g5_bank(models.Model):
     balance = fields.Float(compute="_value_pc", store=True)
     description = fields.Text()
     
-#Many2One    
-    esponsible_id = fields.Many2one('res.users',
-        ondelete='set null', string="Responsible"
-
-        )
+    #Many2One    
+    movement_id = fields.Many2one('res.users',
+        ondelete='set null', string="Movement")
+#
 #
 #     @api.depends('value')
 #     def _value_pc(self):
