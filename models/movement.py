b2ebfd443f6622ea1b2330b2321503cc9bfeefc7
miguel <miturin09@gmail.com>
2026-02-03 12:29:01 +0100

    Merge g5_bank/dev_miguel into dev_miguel
    Conflicts:
    	models/movement.py

--- models/movement.py
+++ models/movement.py
@@ -9,11 +9,20 @@ class Movement(models.Model):
     _name = 'g5_bank.movement'
     _description = 'Movements'
     
+    urrency_id = fields.Many2one('res.currency', string='Currency', 
+                                  default=lambda self: self.env.company.currency_id)
 
-    timesStamp = fields.Char()
-    amount = fields.Integer()
-    balance = fields.Float(compute="_value_pc", store=True)
-    description = fields.Text()
+    timesStamp = fields.Datetime(string='Date', default=fields.Datetime.now)
+    
+    amount = fields.Monetary(string='Amount', currency_field='currency_id', default=0.0)
+    
+    balance = fields.Monetary(string='Balance', 
+        compute='_compute_balance', currency_field='currency_id', default=0.0, readonly=True)
+        
+    description = fields.Selection([
+                                   ('DEPOSIT', 'Deposit'),
+                                   ('PAYMENT', 'Payment'),
+                                   ], string='Movement Type', required=True, default='')
     
     #Many2One    
     g5_account_id = fields.Many2one('g5_bank.account', string="Account")
